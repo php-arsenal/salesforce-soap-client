2eaa7fdfe891376a1d93f4073511017e508e4c47
Evan Shaw <evan.shaw@lightspeedhq.com>
2022-06-21 09:24:56 +1200

    Add default environment for SoapClientFactory::factory
    If we don't have this, $soapOptions is implicitly required, and this
    issues a warning under PHP 8.1.

--- src/Soap/SoapClientFactory.php
+++ src/Soap/SoapClientFactory.php
@@ -57,7 +57,7 @@ class SoapClientFactory
      * @param string $environment
      * @return SoapClient
      */
-    public function factory($wsdl, array $soapOptions = array(), $environment)
+    public function factory($wsdl, array $soapOptions = array(), $environment = 'prod')
     {
         $defaults = array(
             'trace'      => 1,
