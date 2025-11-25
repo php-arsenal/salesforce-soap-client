c0dfcd16995af6d0ec23e16aed3de0f69d6d6007
Gyvastis <shibalba1@gmail.com>
2025-11-25 12:50:47 +0100

    Allow nullable parameters in ClientBuilder

--- src/ClientBuilder.php
+++ src/ClientBuilder.php
@@ -12,10 +12,10 @@ use Psr\Log\LoggerInterface;
  */
 class ClientBuilder
 {
-    protected string $wsdl;
-    protected string $username;
-    protected string $password;
-    protected string $token;
+    protected ?string $wsdl;
+    protected ?string $username;
+    protected ?string $password;
+    protected ?string $token;
     protected array $soapOptions;
     protected string $environment;
 
